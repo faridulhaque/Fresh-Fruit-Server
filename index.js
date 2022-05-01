16bb093cb90a17097079ca1460dadee6e2fafb42
faridulhaque <faridulhaquemurshed@gmail.com>
2022-05-01 17:27:53 +0600

    fetching single data added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // ----------using middleware--------------------------------
@@ -23,6 +23,12 @@ async function run(){
             const cursor = fruitCollection.find(query);
             const fruits = await cursor.toArray();
             res.send(fruits);
+        })
+        app.get('/fruit/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const fruit = await fruitCollection.findOne(query);
+            res.send(fruit);
         })
         
     }
